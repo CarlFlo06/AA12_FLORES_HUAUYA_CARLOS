a42c3b9e1c8c356f78319a8fde50777e57efe700
CarlFlo06 <carlos.d.floresh66@gmail.com>
2023-01-14 18:54:21 -0500

    version 1

---
+++ src/test/java/com/example/tarea12/Tarea12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tarea12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tarea12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
